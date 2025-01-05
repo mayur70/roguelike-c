1b9b8227018df291d5cb9d2238a30288b03f9bf3
Mayur Patil <mayurpatil70@gmail.com>
2025-01-05 15:44:04 +0530

    20-player-enemy-turn-mgmt

--- src/main.c
+++ src/main.c
@@ -27,7 +27,13 @@
 #define DESIRED_FPS 20.0
 #define DESIRED_FRAME_RATE ((1.0 / DESIRED_FPS) * 1000.0)
 
-typedef struct rg_game_state
+typedef enum rg_game_state
+{
+    ST_TURN_PLAYER,
+    ST_TURN_ENEMY
+} rg_game_state;
+
+typedef struct rg_game_state_data
 {
     int screen_width;
     int screen_height;
@@ -47,62 +53,77 @@ typedef struct rg_game_state
     rg_map game_map;
     rg_fov_map fov_map;
     bool recompute_fov;
-} rg_game_state;
+    rg_game_state game_state;
+} rg_game_state_data;
 
-void game_recompute_fov(rg_game_state* state)
+void game_recompute_fov(rg_game_state_data* data)
 {
-    fov_map_compute(&state->fov_map,
-                    state->entities.data[state->player].x,
-                    state->entities.data[state->player].y,
-                    state->fov_radius,
-                    state->fov_light_walls);
+    fov_map_compute(&data->fov_map,
+                    data->entities.data[data->player].x,
+                    data->entities.data[data->player].y,
+                    data->fov_radius,
+                    data->fov_light_walls);
 }
 
-void update(rg_app* app, SDL_Event* event, rg_game_state* state)
+void update(rg_app* app, SDL_Event* event, rg_game_state_data* data)
 {
-    rg_map* game_map = &state->game_map;
-    rg_fov_map* fov_map = &state->fov_map;
+    rg_map* game_map = &data->game_map;
+    rg_fov_map* fov_map = &data->fov_map;
     rg_action action;
     event_dispatch(event, &action);
 
     if (action.type == ACTION_NONE) return;
 
-    if (action.type == ACTION_MOVEMENT)
+    if (action.type == ACTION_MOVEMENT && data->game_state == ST_TURN_PLAYER)
     {
-        int destination_x = state->entities.data[state->player].x + action.dx;
-        int destination_y = state->entities.data[state->player].y + action.dy;
+        int destination_x = data->entities.data[data->player].x + action.dx;
+        int destination_y = data->entities.data[data->player].y + action.dy;
         if (!map_is_blocked(game_map, destination_x, destination_y))
         {
             rg_entity* target = NULL;
             entity_get_at_loc(
-              &state->entities, destination_x, destination_y, &target);
+              &data->entities, destination_x, destination_y, &target);
 
             if (target == NULL)
             {
                 entity_move(
-                  &state->entities.data[state->player], action.dx, action.dy);
-                state->recompute_fov = true;
+                  &data->entities.data[data->player], action.dx, action.dy);
+                data->recompute_fov = true;
             }
             else
             {
                 SDL_Log("You kick the '%s' in shins, much to its annoyance!",
                         target->name);
             }
+            data->game_state = ST_TURN_ENEMY;
         }
     }
 
-    if (state->recompute_fov) game_recompute_fov(state);
+    if (data->recompute_fov) game_recompute_fov(data);
 
     if (action.type == ACTION_ESCAPE || action.type == ACTION_QUIT)
         app->running = false;
+
+    if (data->game_state == ST_TURN_ENEMY)
+    {
+        for (int i = 0; i < data->entities.len; i++)
+        {
+            if (i != data->player)
+            {
+                SDL_Log("The '%s' ponder the meaning of its existance.",
+                        data->entities.data[i].name);
+            }
+        }
+        data->game_state = ST_TURN_PLAYER;
+    }
 }
 
-void draw(rg_app* app, rg_game_state* state)
+void draw(rg_app* app, rg_game_state_data* data)
 {
-    rg_map* game_map = &state->game_map;
-    rg_fov_map* fov_map = &state->fov_map;
-    rg_console* console = &state->console;
-    rg_entity_array* entities = &state->entities;
+    rg_map* game_map = &data->game_map;
+    rg_fov_map* fov_map = &data->fov_map;
+    rg_console* console = &data->console;
+    rg_entity_array* entities = &data->entities;
 
     SDL_SetRenderDrawColor(app->renderer, 0, 0, 0, 255);
     SDL_RenderClear(app->renderer);
@@ -136,7 +157,7 @@ void draw(rg_app* app, rg_game_state* state)
     }
     SDL_RenderPresent(app->renderer);
 
-    state->recompute_fov = false;
+    data->recompute_fov = false;
 }
 
 int main(int argc, char* argv[])
@@ -150,73 +171,74 @@ int main(int argc, char* argv[])
     rg_app app;
     app_create(&app);
 
-    rg_game_state state;
-    state.screen_width = 80;
-    state.screen_height = 50;
-    state.map_width = 80;
-    state.map_height = 45;
-    state.room_max_size = 10;
-    state.room_min_size = 6;
-    state.max_rooms = 30;
-    state.max_monsters_per_room = 3;
-    state.fov_light_walls = true;
-    state.fov_radius = 10;
+    rg_game_state_data data;
+    data.screen_width = 80;
+    data.screen_height = 50;
+    data.map_width = 80;
+    data.map_height = 45;
+    data.room_max_size = 10;
+    data.room_min_size = 6;
+    data.max_rooms = 30;
+    data.max_monsters_per_room = 3;
+    data.fov_light_walls = true;
+    data.fov_radius = 10;
+    data.game_state = ST_TURN_PLAYER;
 
     tileset_create(
-      &state.tileset, app.renderer, "res/dejavu10x10_gs_tc.png", 32, 8);
+      &data.tileset, app.renderer, "res/dejavu10x10_gs_tc.png", 32, 8);
 
-    terminal_create(&state.terminal,
+    terminal_create(&data.terminal,
                     &app,
-                    state.screen_width,
-                    state.screen_height,
-                    &state.tileset,
+                    data.screen_width,
+                    data.screen_height,
+                    &data.tileset,
                     "roguelike",
                     true);
 
-    console_create(&state.console,
+    console_create(&data.console,
                    app.renderer,
-                   state.terminal.width,
-                   state.terminal.height,
-                   state.terminal.tileset);
+                   data.terminal.width,
+                   data.terminal.height,
+                   data.terminal.tileset);
 
-    state.entities.capacity = state.max_monsters_per_room;
-    state.entities.data =
-      malloc(sizeof(*state.entities.data) * state.entities.capacity);
-    ASSERT_M(state.entities.data != NULL);
-    state.entities.len = 0;
-    ARRAY_PUSH(&state.entities,
+    data.entities.capacity = data.max_monsters_per_room;
+    data.entities.data =
+      malloc(sizeof(*data.entities.data) * data.entities.capacity);
+    ASSERT_M(data.entities.data != NULL);
+    data.entities.len = 0;
+    ARRAY_PUSH(&data.entities,
                ((rg_entity){ .x = 0,
                              .y = 0,
                              .ch = '@',
                              .color = WHITE,
                              .name = "player",
                              .blocks = true }));
-    state.player = state.entities.len - 1;
+    data.player = data.entities.len - 1;
 
-    map_create(&state.game_map,
-               state.map_width,
-               state.map_height,
-               state.room_min_size,
-               state.room_max_size,
-               state.max_rooms,
-               state.max_monsters_per_room,
-               &state.entities,
-               state.player);
+    map_create(&data.game_map,
+               data.map_width,
+               data.map_height,
+               data.room_min_size,
+               data.room_max_size,
+               data.max_rooms,
+               data.max_monsters_per_room,
+               &data.entities,
+               data.player);
 
-    fov_map_create(&state.fov_map, state.map_width, state.map_height);
-    for (int y = 0; y < state.map_height; y++)
+    fov_map_create(&data.fov_map, data.map_width, data.map_height);
+    for (int y = 0; y < data.map_height; y++)
     {
-        for (int x = 0; x < state.map_width; x++)
+        for (int x = 0; x < data.map_width; x++)
         {
-            rg_tile* tile = map_get_tile(&state.game_map, x, y);
+            rg_tile* tile = map_get_tile(&data.game_map, x, y);
             fov_map_set_props(
-              &state.fov_map, x, y, !tile->block_sight, !tile->blocked);
+              &data.fov_map, x, y, !tile->block_sight, !tile->blocked);
         }
     }
 
     // first draw before waitevent
-    game_recompute_fov(&state);
-    draw(&app, &state);
+    game_recompute_fov(&data);
+    draw(&app, &data);
 
     while (app.running)
     {
@@ -225,8 +247,8 @@ int main(int argc, char* argv[])
 
         Uint64 frame_start = SDL_GetPerformanceCounter();
 
-        update(&app, &event, &state);
-        draw(&app, &state);
+        update(&app, &event, &data);
+        draw(&app, &data);
 
         Uint64 frame_end = SDL_GetPerformanceCounter();
         double dt = (frame_end - frame_start) /
@@ -239,11 +261,11 @@ int main(int argc, char* argv[])
         SDL_Delay(sleep_time);
     }
 
-    map_destroy(&state.game_map);
-    free(state.entities.data);
-    console_destroy(&state.console);
-    terminal_destroy(&state.terminal);
-    tileset_destroy(&state.tileset);
+    map_destroy(&data.game_map);
+    free(data.entities.data);
+    console_destroy(&data.console);
+    terminal_destroy(&data.terminal);
+    tileset_destroy(&data.tileset);
 
     app_destroy(&app);
     return 0;
