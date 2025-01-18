36ac225ec37a29af55707f5583f6d29a381bd04f
Mayur Patil <mayurpatil70@gmail.com>
2025-01-18 19:20:48 +0530

    35-show-inventory-4

--- src/main.c
+++ src/main.c
@@ -144,8 +144,25 @@ void menu_draw(rg_console* c,
     int header_height = (int)ceil((double)len / (double)width);
     *height = options_count + header_height;
 
-    console_print_txt(c, 0, 0, header, WHITE);
-    int y = header_height;
+    int y = 0;
+    {
+        char* buf = malloc(sizeof(char) * width + 1);
+        size_t len_remaining = len;
+        for (int i = 0; i < header_height; i++)
+        {
+            // TODO: Split by word?
+            size_t sz;
+            if (len_remaining > width) sz = width;
+            else
+                sz = len_remaining;
+            memcpy(buf, header + (width * i), sz);
+            buf[sz] = '\0';
+            console_print_txt(c, 0, y, buf, WHITE);
+            y++;
+            len_remaining -= sz;
+        }
+        free(buf);
+    }
     int letter_index = (char)'a';
     for (int i = 0; i < options_count; i++)
     {
